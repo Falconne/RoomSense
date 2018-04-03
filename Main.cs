9a20863125e230b469544b4009042a5f4907144b
Anuradha Dissanayake <falconne@gmail.com>
2018-04-04 11:13:16 +1200

    Add skeleton

---
+++ Main.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using HugsLib.Settings;
+using HugsLib.Utils;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+
+namespace RoomSense
+{
+    public class Main : HugsLib.ModBase
+    {
+        public Main()
+        {
+            Instance = this;
+        }
+
+        public void UpdateOverlay()
+        {
+        }
+
+        public override void OnGUI()
+        {
+            if (Current.ProgramState != ProgramState.Playing || Find.VisibleMap == null
+                || WorldRendererUtility.WorldRenderedNow)
+            {
+                return;
+            }
+
+        }
+
+        public override void WorldLoaded()
+        {
+            //Reset();
+        }
+
+        public override void DefsLoaded()
+        {
+            _opacity = Settings.GetHandle(
+                "opacity", "FALCHM.OverlayOpacity".Translate(),
+                "FALCHM.OverlayOpacityDesc".Translate(), 30,
+                Validators.IntRangeValidator(1, 100));
+
+            // _opacity.OnValueChanged = val => { Reset(); };
+
+            _updateDelay = Settings.GetHandle("updateDelay", "FALCRS.UpdateDelay".Translate(),
+                "FALCRS.UpdateDelayDesc".Translate(),
+                100, Validators.IntRangeValidator(1, 9999));
+
+        }
+
+        public float GetConfiguredOpacity()
+        {
+            return _opacity / 100f;
+        }
+
+        public int GetUpdateDelay()
+        {
+
+            return _updateDelay;
+        }
+
+        internal new ModLogger Logger => base.Logger;
+
+        internal static Main Instance { get; private set; }
+
+        public override string ModIdentifier => "RoomSense";
+
+        private SettingHandle<int> _opacity;
+
+        private SettingHandle<int> _updateDelay;
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Room Sense Rimworld Mod")]
+[assembly: AssemblyDescription("Displays room quality overlay over applicable rooms")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Room Sense")]
+[assembly: AssemblyCopyright("Copyright © Anuradha Dissanayake  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c45f179-0329-4b3e-a9b4-ef2ac8cedc5c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.18.1.0")]
+[assembly: AssemblyFileVersion("0.18.1.0")]
