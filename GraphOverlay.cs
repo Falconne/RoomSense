2157a149c0a5eb6010e30423317184e7202de7e7
Anuradha Dissanayake <falconne@gmail.com>
2018-04-07 00:14:56 +1200

    Use fixed color meters

--- GraphOverlay.cs
+++ GraphOverlay.cs
@@ -7,30 +7,6 @@ namespace RoomSense
 {
     public class GraphOverlay
     {
-        private List<Color> _barColorGradients = new List<Color>();
-
-        public GraphOverlay()
-        {
-            var maxPossibleLevel = 0;
-            foreach (var statDef in DefDatabase<RoomStatDef>.AllDefsListForReading)
-            {
-                if (statDef.isHidden)
-                    continue;
-
-                if (statDef.scoreStages.Count > maxPossibleLevel)
-                    maxPossibleLevel = statDef.scoreStages.Count;
-            }
-
-            var delta = .4f / maxPossibleLevel;
-            var hue = 0f;
-            for (var i = 0; i < maxPossibleLevel; i++)
-            {
-                var color = Color.HSVToRGB(hue, 1f, 1f);
-                _barColorGradients.Add(color);
-                hue += delta;
-            }
-        }
-
         public void OnGUI(InfoCollector infoCollector)
         {
             if (!infoCollector.IsValid())
@@ -61,12 +37,12 @@ namespace RoomSense
                 var meterDrawY = drawTopLeft.y + margin;
                 foreach (var infoStat in roomInfo.Stats)
                 {
-                    var barColorIndexScaler = _barColorGradients.Count / (float) infoStat.MaxLevel;
-                    var barColorIndex = (int) (infoStat.CurrentLevel * barColorIndexScaler);
-                    if (barColorIndex >= _barColorGradients.Count)
-                        barColorIndex = _barColorGradients.Count - 1;
-
-                    var barColor = _barColorGradients[barColorIndex];
+                    var currentLevelFraction = (infoStat.CurrentLevel + 1f) / infoStat.MaxLevel;
+                    var barColor = Color.green;
+                    if (currentLevelFraction < .66f)
+                        barColor = Color.yellow;
+                    if (currentLevelFraction < .33)
+                        barColor = Color.red;
 
                     var meterDrawX = drawTopLeft.x + margin * 2 + iconWidth;
                     for (var i = 0; i < infoStat.MaxLevel; i++, meterDrawX += barLength)
