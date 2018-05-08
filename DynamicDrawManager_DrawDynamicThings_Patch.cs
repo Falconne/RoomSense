30a1e7b212c63f7e91f7406e3e57c5c4103a3924
Anuradha Dissanayake <falconne@gmail.com>
2018-05-09 09:01:17 +1200

    Create test mesh

--- DynamicDrawManager_DrawDynamicThings_Patch.cs
+++ DynamicDrawManager_DrawDynamicThings_Patch.cs
@@ -11,6 +11,8 @@ namespace RoomSense
     [HarmonyPatch(typeof(DynamicDrawManager), "DrawDynamicThings")]
     public class DynamicDrawManager_DrawDynamicThings_Patch
     {
+        private static Mesh _testMesh;
+
         static bool Prefix(ref DynamicDrawManager __instance)
         {
             var map = Find.VisibleMap;
@@ -26,6 +28,9 @@ namespace RoomSense
                     continue;
 
                 var topLeft = room.BorderCells.First();
+                if (_testMesh == null)
+                    CreateTestMesh();
+
                 var s = new Vector3(.5f, 1f, .5f);
                 Matrix4x4 matrix = default;
                 var pos = sectionCell.ToVector3();
@@ -33,11 +38,50 @@ namespace RoomSense
                 pos.z -= .5f;
                 matrix.SetTRS(pos, Quaternion.identity, s);
 
-                Graphics.DrawMesh(MeshPool.plane10, matrix, mat, 0);
+                Graphics.DrawMesh(_testMesh, matrix, mat, 0);
             }
 
             return true;
         }
+
+        public static void CreateTestMesh()
+        {
+            Vector3[] array = new Vector3[4];
+            Vector2[] array2 = new Vector2[4];
+            var size = new Vector2
+            {
+                x = 1f,
+                y = 1f
+            };
+
+            int[] array3 = new int[6];
+            array[0] = new Vector3(-0.5f * size.x, 0f, -0.5f * size.y);
+            array[1] = new Vector3(-0.5f * size.x, 0f, 0.5f * size.y);
+            array[2] = new Vector3(0.5f * size.x, 0f, 0.5f * size.y);
+            array[3] = new Vector3(0.5f * size.x, 0f, -0.5f * size.y);
+
+            array2[0] = new Vector2(1f, 0f);
+            array2[1] = new Vector2(1f, 1f);
+            array2[2] = new Vector2(0f, 1f);
+            array2[3] = new Vector2(0f, 0f);
+
+            array3[0] = 0;
+            array3[1] = 1;
+            array3[2] = 2;
+            array3[3] = 0;
+            array3[4] = 2;
+            array3[5] = 3;
+            _testMesh = new Mesh
+            {
+                name = "NewPlaneMesh()",
+                vertices = array,
+                uv = array2
+            };
+            _testMesh.SetTriangles(array3, 0);
+            _testMesh.RecalculateNormals();
+            _testMesh.RecalculateBounds();
+        }
+
     }
 
 }
